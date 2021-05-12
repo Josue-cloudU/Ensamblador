751b48711b925ccb0654fd9b89373c68e870292f
Josue-cloudU <75402445+Josue-cloudU@users.noreply.github.com>
2021-05-12 16:00:18 -0700

    Update calc.asm

--- calc.asm
+++ calc.asm
@@ -2,11 +2,10 @@
 .stack
 .data
 n1 db 0
-n2 db 0
 r db 0
 msg db 10,13,7,' x ','$'
 msg0 db 10,13,7,'Ingresa el numero:','$'
-conta db 10
+conta db 1
 
 .code
 mov ax, seg @data
@@ -20,17 +19,6 @@ mov ah, 01h    ;captura del numero 1
 int 21h
 sub al, 30h
 mov n1,al
-
-mov ah, 09h
-lea dx, msg0
-int 21h
-
-mov ah, 01h    ;captura del numero 2
-int 21h
-sub al, 30h
-mov n2,al
-
-
 ;--------------------------------------------------------------------------------
 
 ciclo:
